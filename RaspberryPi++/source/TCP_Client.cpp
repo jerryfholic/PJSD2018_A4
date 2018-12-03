5604e37040f88fbf0f040481013e4579078557a1
CryingEagle1 <m.gatsonides@hotmail.com>
2018-12-03 17:13:51 +0100

    new: dev: Added TCP_Client class to RaspberiPi++

---
+++ RaspberryPi++/source/TCP_Client.cpp
@@ -0,0 +1,13 @@
+/*
+ * TCP_Client.cpp
+ *
+ *  Created on: Dec 3, 2018
+ *      Author: programmer
+ */
+
+#include "TCP_Client.h"
+
+void TCP_Client::createSocket()
+{
+
+}
---
+++ RaspberryPi++/source/TCP_Client.h
@@ -0,0 +1,26 @@
+/*
+ * TCP_Client.h
+ *
+ *  Created on: Dec 3, 2018
+ *      Author: programmer
+ */
+
+#ifndef TCP_CLIENT_H_
+#define TCP_CLIENT_H_
+
+class TCP_Client
+{
+public:
+	void createSocket();
+	void sendString();
+	void sendChar();
+	void sendInt();
+
+private:
+
+
+};
+
+
+
+#endif /* TCP_CLIENT_H_ */
