04051c907df29406116a51fa5a285c0c37a7fd23
CryingEagle1 <m.gatsonides@hotmail.com>
2018-12-10 15:14:04 +0100

    new: dev: Added TCP_Client class

--- RaspberryPi++/source/TCP_Client.cpp
+++ RaspberryPi++/source/TCP_Client.cpp
@@ -7,7 +7,70 @@
 
 #include "TCP_Client.h"
 
+TCP_Client::TCP_Client(const char* address, int poort)
+{
+	ip_address = address;
+	port = poort;
+	TCP_socket();
+	createSocket();
+}
+
+void TCP_Client::TCP_socket()
+{
+	sock = -1;
+}
+
 void TCP_Client::createSocket()
 {
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == -1)
+	{
+		//TODO Socket creating error
+	}
+	cout  << "Socket created\n";
+}
 
+bool TCP_Client::connectToServer(string address, int)
+{
+	//create socket if it isnt created yet!
+	if(sock == -1)
+	{
+		createSocket();
+	}
+	else{}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+
+
+
+	//IP address conversion to binary
+	if(inet_pton(AF_INET, "192.168.4.3", &serv_addr.sin_addr)<=0)
+	    {
+	        printf("\nInvalid address/ Address not supported \n");
+	        return -1;
+	    }
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	    {
+	        printf("\nConnection Failed \n");
+	        return -1;
+	    }
+	return 0;
+}
+
+
+bool TCP_Client::sendString(char const *str)
+{
+	send(sock, str, strlen(str), 0);
+	return 0;
+}
+
+void TCP_Client::receive()
+{
+	valread = read(sock, buffer, 1024);
+	printf("%s\n",buffer);
+	buffer[1024] = {0};
 }
--- RaspberryPi++/source/TCP_Client.h
+++ RaspberryPi++/source/TCP_Client.h
@@ -8,19 +8,36 @@
 #ifndef TCP_CLIENT_H_
 #define TCP_CLIENT_H_
 
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+
+
+using namespace std;
+
 class TCP_Client
 {
 public:
+	TCP_Client(const char *address, int port);
+	void TCP_socket();
 	void createSocket();
-	void sendString();
-	void sendChar();
-	void sendInt();
+	bool connectToServer(string, int);
+	bool sendString(char const *str);
+
+	void receive();
 
 private:
-
+	int sock;
+	int port;
+	struct sockaddr_in serv_addr;
+	const char *ip_address;
+	char buffer[1024];
+	int valread;
 
 };
-
-
 
 #endif /* TCP_CLIENT_H_ */
